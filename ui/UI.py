718ffb2b200dcafb4ebbe9a582a07e8eabd01844
275751-ops <275751@student.pwr.edu.pl>
2026-05-16 13:10:55 +0200

    UI_labelka_Z_Bledem

--- ui/UI.py
+++ ui/UI.py
@@ -23,18 +23,26 @@ class SensorApp(QWidget):
 
         # Pole adresu API
         self.api_input = QLineEdit()
-        self.api_input.setText("http://127.0.0.1:5001")
+        self.api_input.setText("http://192.168.0.57:5001")
         layout.addWidget(QLabel("Adres API:"))
         layout.addWidget(self.api_input)
 
         # Przyciski i chkboxy
+        self.status_label = QLabel("Status: Rozłączony")
+        layout.addWidget(self.status_label)
+
         self.connect_button = QPushButton("Połącz")
         self.connect_button.clicked.connect(self.update_charts)
         button_layout.addWidget(self.connect_button)
 
+
         self.pomiar = QPushButton("Pomiar")
-        self.pomiar.clicked.connect(self.obsluga_klikniecia)
+        self.pomiar.clicked.connect(self.Pomiar)
         button_layout.addWidget(self.pomiar)
+
+        self.clear = QPushButton("wyczyść wykresy")
+        self.clear.clicked.connect(self.clear_chart)
+        button_layout.addWidget(self.clear)
 
         self.ciagly_pomiar = QCheckBox("Ciągły pomiar")
         self.ciagly_pomiar.setChecked(False)
@@ -76,7 +84,7 @@ class SensorApp(QWidget):
     # def start_fetching(self):
     #     self.timer.start(2000)  # co 2 sekundy
     #     self.update_charts()
-    def obsluga_klikniecia(self):
+    def Pomiar(self):
         self.update_charts()
 
     def get_sensor_data(self, sensor_name):
@@ -91,6 +99,7 @@ class SensorApp(QWidget):
             response = requests.get(url, timeout=3)
 
             if response.status_code == 200:
+                self.status_label.setText("Status: Połączono")
                 data = response.json()
 
                 data.reverse()
@@ -100,43 +109,58 @@ class SensorApp(QWidget):
 
                 return times, values
 
-        except Exception as e:
-            print(e)
+        except requests.exceptions.ConnectionError:
+            self.status_label.setText("Błąd połączenia - zły adres API")
+
+        except requests.exceptions.Timeout:
+            self.status_label.setText("Timeout - serwer nie odpowiada")
+
+        except requests.exceptions.HTTPError as e:
+            self.status_label.setText(f"Błąd HTTP: {e}")
+
+        except requests.exceptions.RequestException as e:
+            self.status_label.setText(f"Inny błąd requests: {e}")
+
+        except ValueError:
+            self.status_label.setText("Błędny JSON z serwera")
 
         return [], []
-
-    def update_charts(self):
-
-        # Temperatura
-        t_x, t_y = self.get_sensor_data("temperature")
-
-        # Ciśnienie
-        p_x, p_y = self.get_sensor_data("pressure")
-
-        # Wysokość
-        a_x, a_y = self.get_sensor_data("Altitude")
-
-        # Czyszczenie
+    
+    def clear_chart(self):
+        #print(self.t_x, self.t_y)
         self.ax_temp.clear()
         self.ax_pressure.clear()
         self.ax_altitude.clear()
+        self.canvas.draw_idle()
 
-        # Rysowanie
+    def update_charts(self):
+        # Temperatura
+        t_x, t_y = self.get_sensor_data("temperature")
+        # cisnienie
+        p_x, p_y = self.get_sensor_data("pressure")
+        # wysokosc
+        a_x, a_y = self.get_sensor_data("Altitude")
+
+        # jeśli brak danych → nie rysuj
+        if not t_y and not p_y and not a_y:
+            print("Brak danych")
+            self.clear_chart()
+            self.canvas.draw_idle()
+            return
+
+        self.clear_chart()
+
         self.ax_temp.plot(t_x, t_y)
         self.ax_temp.set_title("Temperatura")
-        self.ax_temp.grid(True)
 
         self.ax_pressure.plot(p_x, p_y)
         self.ax_pressure.set_title("Ciśnienie")
-        self.ax_pressure.grid(True)
 
         self.ax_altitude.plot(a_x, a_y)
         self.ax_altitude.set_title("Wysokość")
-        self.ax_altitude.grid(True)
 
         self.figure.tight_layout()
-
-        self.canvas.draw()
+        self.canvas.draw_idle()
 
 
 app = QApplication(sys.argv)
